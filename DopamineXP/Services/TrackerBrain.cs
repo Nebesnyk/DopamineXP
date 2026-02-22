de776312fb901be75fc16356d729bde6c7a06406
Darion <darionmamyko@gmail.com>
2026-02-21 22:32:03 -0700

    Added delete and edit task buttons. Adjusted home.razor page for phones

--- DopamineXP/Services/TrackerBrain.cs
+++ DopamineXP/Services/TrackerBrain.cs
@@ -7,24 +7,6 @@ using System.IO;
 
 public class TrackerBrain
 {
-    public double ProgrammingXp { get; set; } = 0;
-    public int ProgrammingLevel { get; set; } = 1;
-    public int ProgrammingStreak { get; set; } = 1;
-    public int ProgrammingPoints { get; set; } = 0;
-    public DateTime LastProgrammingLog { get; set; } = DateTime.MinValue;
-    
-    public double GymXp { get; set; } = 0;
-    public int GymLevel { get; set; } = 1;
-    public int GymStreak { get; set; } = 1;
-    public int GymPoints { get; set; } = 0;
-    public DateTime LastGymLog { get; set; } = DateTime.MinValue;
-    
-    public double LanguagesXp { get; set; } = 0;
-    public int LanguagesLevel { get; set; } = 1;
-    public int LanguagesStreak { get; set; } = 1;
-    public int LanguagesPoints { get; set; } = 0;
-    public DateTime LastLanguagesLog { get; set; } = DateTime.MinValue;
-
     public List<DopamineTask> CustomTasks { get; set; } = new();
     public Action? OnChange;
     
@@ -63,25 +45,10 @@ public class TrackerBrain
             return;
         string jsonString = File.ReadAllText(appFile);
 
-        TrackerBrain loadedBrain = JsonSerializer.Deserialize<TrackerBrain>(jsonString);
+        TrackerBrain? loadedBrain = JsonSerializer.Deserialize<TrackerBrain>(jsonString);
 
-        this.ProgrammingXp = loadedBrain.ProgrammingXp;
-        this.ProgrammingLevel = loadedBrain.ProgrammingLevel;
-        this.ProgrammingStreak = loadedBrain.ProgrammingStreak;
-        this.ProgrammingPoints = loadedBrain.ProgrammingPoints;
-        this.LastProgrammingLog = loadedBrain.LastProgrammingLog;
-        
-        this.GymXp = loadedBrain.GymXp;
-        this.GymLevel = loadedBrain.GymLevel;
-        this.GymStreak = loadedBrain.GymStreak;
-        this.GymPoints = loadedBrain.GymPoints;
-        this.LastGymLog = loadedBrain.LastGymLog;
-        
-        this.LanguagesXp = loadedBrain.LanguagesXp;
-        this.LanguagesLevel = loadedBrain.LanguagesLevel;
-        this.LanguagesStreak = loadedBrain.LanguagesStreak;
-        this.LanguagesPoints = loadedBrain.LanguagesPoints;
-        this.LastLanguagesLog = loadedBrain.LastLanguagesLog;
+        if (loadedBrain != null && loadedBrain.CustomTasks != null)
+            this.CustomTasks = loadedBrain.CustomTasks;
     }
 
     public int CalculateNewStreak(int currentStreak, DateTime lastLogDate)
